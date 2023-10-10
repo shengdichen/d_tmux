396fd823a249c3b06a4f3741781cf86a8595c7c7
shc <sh@c>
2023-10-10 02:58:29 +0200

    Only reopen if in vifm

--- .config/tmux/script/open.sh
+++ .config/tmux/script/open.sh
@@ -1,5 +1,9 @@
 session_path="${HOME}/.local/state/nvim/throw_session.vim"
 
+function should_respawn() {
+    [[ $(tmux display-message -p -F "#{pane_current_command}") == "vifm" ]]
+}
+
 function make_session() {
     tmux send-keys ":mksession! ${session_path} || qa" "Enter"
 }
@@ -9,11 +13,13 @@ function respawn() {
 }
 
 function main() {
-    make_session
-    respawn
+    if should_respawn; then
+        make_session
+        respawn
+    fi
 
     unset session_path
-    unset -f make_session respawn
+    unset -f should_respawn make_session respawn
 }
 main
 unset -f main
